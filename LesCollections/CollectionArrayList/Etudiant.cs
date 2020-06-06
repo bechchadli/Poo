c457898a03961e805e7e82528b95dde5940d1194
bechchadli <35851668+bechchadli@users.noreply.github.com>
2020-06-06 13:05:43 +0100

    Add files via upload

---
+++ LesCollections/CollectionArrayList/Etudiant.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExempleCollection
+{
+    [Serializable]
+    class Etudiant
+    {
+        private string ni;
+        private string nom;
+        private string prenom;
+        private string filiere;
+
+        private List<Note> Notes = new List<Note>();
+        
+        private Note oN;
+
+        public string Ni
+        {
+            get { return ni; }
+            set { ni = value; }
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+        public string Filiere
+        {
+            get { return filiere; }
+            set { filiere    = value; }
+        }
+        
+        //Constructeur
+        public Etudiant()
+        {
+
+        }
+
+        public Etudiant(string ni, string nom, string prenom, string filiere)
+        {
+            this.ni = ni;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.filiere = filiere;
+        }
+
+        public void saisrValeur()
+        {
+            Console.Write("Saisir le numInscription : "); ni = Console.ReadLine();
+            Console.Write("Saisir le Nom : "); nom = Console.ReadLine();
+            Console.Write("Saisir le Prenom : "); prenom = Console.ReadLine();
+            Console.Write("Saisir la Filière : "); filiere = Console.ReadLine();
+        }
+
+        public void ajouterNote()
+        {
+                oN = new Note(); //instancier un objet note
+                oN.saisrValeur(); //Remplir les info de la nouvelle note
+                Notes.Add(oN); //affecter la nouvelle note à notre collection de note            
+        }
+
+        public float moyenne()
+        {
+            float s = 0; int sc = 0;
+
+            //for (int i = 0; i < Notes.Count(); i++)
+            //{
+            //    oN = Notes[i];
+            //    s = s + oN.mc();
+            //    sc = sc + oN.Coeff;
+            //}
+
+            foreach(Note n in Notes)
+            {
+                s = s + n.mc();
+                sc = sc + n.Coeff;
+            }
+            if (sc == 0)
+            {
+                Console.WriteLine("Attention à la division sur 0 si votre étudiant n'a aucune note");
+                return 0;
+            }
+            return s / sc;//Attention à la division sur 0 si votre étudiant n'a aucune note
+        }
+
+        public void afficherValeur()
+        {
+            Console.WriteLine("Le num Etu : {0}",ni);
+            Console.WriteLine("Le nom Etu : {0}",nom);
+            Console.WriteLine("Le prenom Etu : {0}", prenom);
+            Console.WriteLine("La filiere Etu : {0}",filiere);
+            Console.WriteLine("La moyenne Etu : {0}",moyenne().ToString());
+        }
+
+        public void supprimerNote()
+        {
+            for (int i = 0; i < Notes.Count; i++)
+            {
+                Console.WriteLine("{0} - {1} note : {2}", i + 1, Notes[i].Matiere, Notes[i].Valeur);
+            }
+
+            Console.Write("Saisir le num Note à supprimer : "); int rep = int.Parse( Console.ReadLine());
+
+            
+            Notes.RemoveAt(rep-1);
+        }
+
+        public void relever()
+        {
+            Console.WriteLine("NI : {0} \t Nom : {1} \t Prenom : {2} \n", ni, nom, prenom);
+            Console.WriteLine("Matiere \t Note \t Coeff \t NoteCoeff");
+             foreach (Note n in Notes)
+            {
+               Console.WriteLine("{0} \t\t {1} \t {2} \t {3}",n.Matiere,n.Valeur,n.Coeff,n.mc());
+            }
+
+             Console.WriteLine("\n\n \t\t Maoyenne Generale : {0}", moyenne());         
+        }
+    }
+}
---
+++ LesCollections/CollectionArrayList/Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace ExempleCollection
+{
+    class Program
+    {
+        static List<Etudiant> Etudiants = new List<Etudiant>();
+        static Dictionary<int, Etudiant> DicEtudiants = new Dictionary<int, Etudiant>();
+        static Hashtable HashEtudiants = new Hashtable();
+        static Etudiant oE;
+
+        static void listerDictionnaire()
+        {
+            foreach (KeyValuePair<int,Etudiant> de in DicEtudiants)
+            {
+                Console.WriteLine(de.Key + "  " + de.Value);
+            }
+
+            foreach (DictionaryEntry de in HashEtudiants)
+            {
+                Console.WriteLine(de.Key + "  " + de.Value);
+            }
+            foreach (Etudiant e in DicEtudiants.Values)
+            {
+                Console.WriteLine(de.Key + "  " + de.Value.Ni);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int choix;
+            Deserialiser();
+            do
+            {
+                choix = menu();
+                Console.Clear();
+
+                switch (choix)
+                {
+                    case 1:
+                        ajouterEtudiant();
+                        break;
+                    case 2:
+                        rechercherEtuMenu();
+                        break;
+                    case 3:
+                        lister();
+                        break;
+                    case 4:
+                        modifierEtu();
+                        break;
+                    case 5:
+                        supprimerEtu();
+                        break;
+                    case 6:
+                        ajouternote();
+                        break;
+                    case 7:
+                        supprimerNote();
+                        break;
+                    case 8:
+                        relever();
+                        break;
+
+
+                }
+                Console.ReadKey();
+            } while (choix != 15);
+            Serialiser();
+        }
+
+        static int menu()
+        {
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("********** Menu ************");
+            Console.WriteLine("****************************");
+            Console.WriteLine(" 1- Ajouter Etudiant       *");
+            Console.WriteLine(" 2- Rechercher Etudiant    *");
+            Console.WriteLine(" 3- Lister Etudiant et Moy *");
+            Console.WriteLine(" 4- Modifier Etudiant      *");
+            Console.WriteLine(" 5- Supprimer Etudiant     *");
+            Console.WriteLine(" 6- Ajouter une note       *");
+            Console.WriteLine(" 7- Supprimer Note         *");
+            Console.WriteLine(" 8- Relever                *");
+            Console.WriteLine(" 9- Le premier             *");
+            Console.WriteLine("10- Le dernier             *");
+            Console.WriteLine("11- Les Admis              *");
+            Console.WriteLine("12- Les eliminés           *");
+            Console.WriteLine("15- Quitter                *");
+            Console.WriteLine("****************************");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void ajouterEtudiant()
+        {
+                oE = new Etudiant();
+                oE.saisrValeur();
+                Etudiants.Add(oE);
+        }
+
+        static void lister()
+        {
+            
+            foreach (Etudiant e in Etudiants)
+            {
+                Console.WriteLine("Information de l'etudiant N° {0} : ", Etudiants.IndexOf(e) + 1);
+                e.afficherValeur();
+                Console.WriteLine("\n");
+            }
+        }
+
+        static Etudiant rechercherEtu()
+        {
+            Console.Write("Saisir le Num Etu : "); string ni = Console.ReadLine();
+            foreach (Etudiant e in Etudiants)
+            {
+                if (ni.Equals(e.Ni)) return e;
+
+            }
+            Console.WriteLine("Cet etudiant n'existe pas !!!!!!!!");
+            return null;
+        }
+
+        static int rechercherPosition()
+        {
+            Console.Write("Saisir le Num Etu : "); string ni = Console.ReadLine();
+
+            //Solution 1
+            //for (int i = 0; i < Etudiants.Count; i++)
+            //{
+            //    oE = Etudiants[i];
+
+            //    if (ni.Equals(oE.Ni)) return i;
+
+            //}
+            //Console.WriteLine("Cet etudiant n'existe pas !!!!!!!!");
+            //return -1;
+
+            //Solution 2
+            foreach (Etudiant e in Etudiants)
+            {
+                if (ni.Equals(e.Ni)) return Etudiants.IndexOf(e);
+
+            }
+            Console.WriteLine("Cet etudiant n'existe pas !!!!!!!!");
+            return -1;
+        }
+
+        static void rechercherEtuMenu()
+        {
+            oE = rechercherEtu();
+            if (oE != null)
+            {
+                oE.afficherValeur();
+            }
+
+        }
+
+        static void modifierEtu()
+        {
+            oE = rechercherEtu();
+            if (oE != null)
+            {
+                oE.afficherValeur();
+                oE.saisrValeur();
+            }
+        }
+
+        static void supprimerEtu()
+        {
+            //solution 1
+            int p = rechercherPosition();
+            if (p != -1) Etudiants.RemoveAt(p);
+            
+            //solution2
+            oE = rechercherEtu();
+            if (p != null) Etudiants.Remove(oE);
+        }
+
+        static void ajouternote()
+        {
+            oE = rechercherEtu();
+            if (oE != null)
+            {
+                oE.afficherValeur();
+                oE.ajouterNote();
+            }
+        }
+
+        static void supprimerNote()
+        {
+            oE = rechercherEtu();
+            if (oE != null)
+            {
+                oE.afficherValeur();
+                oE.supprimerNote();
+            }
+        }
+
+        static void relever()
+        {
+            oE = rechercherEtu();
+            if (oE != null)
+            {
+                oE.relever();
+            }
+        }
+
+        public static void Serialiser()
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream fs = new FileStream("MonFichier.dat", FileMode.Create, FileAccess.Write);
+            formatter.Serialize(fs, Etudiants);
+            fs.Close();
+        }
+        public static void Deserialiser()
+        {
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream fs = new FileStream("MonFichier.dat", FileMode.Open, FileAccess.Read);
+                Etudiants = (List<Etudiant>)formatter.Deserialize(fs);
+                fs.Close();
+                
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Fichier Introuvable!!!!");
+            }
+            
+        }
+    }
+}
